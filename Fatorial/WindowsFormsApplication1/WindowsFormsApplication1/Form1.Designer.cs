4f21f8e9e6741a8d23d187a55eedc082258c85c0
mauriciolfsilva <mauriciolfsilva@yahoo.com.br>
2015-03-06 09:33:37 -0300

    Fatorial_2003_DanielRod_MateusAm_Mauricio
    Um trabalho feito pelos alunos Daniel Oliveira, Mateus Amaral e
    Mauricio, do Colégio Estadual José Leite Lopes - Nave, cuja função é
    calcular o fatorial de um número através de um código Iterativo e um
    Recursivo.

---
+++ Fatorial/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Iterativa = new System.Windows.Forms.Button();
+            this.Recursiva = new System.Windows.Forms.Button();
+            this.NumEnter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Iterativa
+            // 
+            this.Iterativa.Location = new System.Drawing.Point(40, 161);
+            this.Iterativa.Name = "Iterativa";
+            this.Iterativa.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Iterativa.Size = new System.Drawing.Size(75, 23);
+            this.Iterativa.TabIndex = 0;
+            this.Iterativa.Text = "Iterativa";
+            this.Iterativa.UseVisualStyleBackColor = true;
+            this.Iterativa.Click += new System.EventHandler(this.Iterativa_Click);
+            // 
+            // Recursiva
+            // 
+            this.Recursiva.Location = new System.Drawing.Point(151, 161);
+            this.Recursiva.Name = "Recursiva";
+            this.Recursiva.Size = new System.Drawing.Size(75, 23);
+            this.Recursiva.TabIndex = 1;
+            this.Recursiva.Text = "Recursiva";
+            this.Recursiva.UseVisualStyleBackColor = true;
+            this.Recursiva.Click += new System.EventHandler(this.Recursiva_Click);
+            // 
+            // NumEnter
+            // 
+            this.NumEnter.Location = new System.Drawing.Point(87, 89);
+            this.NumEnter.Name = "NumEnter";
+            this.NumEnter.Size = new System.Drawing.Size(100, 20);
+            this.NumEnter.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 261);
+            this.Controls.Add(this.NumEnter);
+            this.Controls.Add(this.Recursiva);
+            this.Controls.Add(this.Iterativa);
+            this.Name = "Form1";
+            this.Text = "Fatorial DanielOliveira,MateusAmaral e Mauricio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Iterativa;
+        private System.Windows.Forms.Button Recursiva;
+        private System.Windows.Forms.TextBox NumEnter;
+    }
+}
+
---
+++ Fatorial/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Iterativa_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(NumEnter.Text);
+            double r = 1;
+
+            for (var i = 1; i <= a; i++)
+            {
+                r *= i;
+            }
+
+            NumEnter.Text = r.ToString();
+        }
+
+        private void Recursiva_Click(object sender, EventArgs e)
+        {
+            NumEnter.Text = recursiva(double.Parse(NumEnter.Text)).ToString();
+        }
+
+        public double recursiva(double n1)
+        {
+            if (n1 == 0)
+                return 1;
+            return recursiva(n1 - 1) * n1;
+        }
+    }
+}
